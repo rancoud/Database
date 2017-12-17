bc43a3aaed0b18b65e841799645384ae80fbc314
Sébastien Rancoud <sebastien.rancoud@gmail.com>
2017-12-18 00:05:17 +0100

    rework

--- src/Configurator.php
+++ src/Configurator.php
@@ -2,9 +2,278 @@
 
 namespace Rancoud\Database;
 
+use Exception;
+use PDO;
+
 /**
- * Interface Configurator.
+ * Class Configurator.
  */
-interface Configurator
+class Configurator
 {
+    protected $engine;
+
+    protected $host;
+
+    protected $user;
+
+    protected $password;
+
+    protected $database;
+
+    protected $parameters;
+
+    protected $saveQueries = false;
+
+    protected $permanentConnection = false;
+
+    protected $reportError = 'exception';
+
+    protected $charset = 'utf8';
+
+    /**
+     * DatabaseConfigurator constructor.
+     *
+     * @param array $settings
+     *
+     * @throws \Exception
+     */
+    public function __construct(array $settings)
+    {
+        $this->setMandatorySettings($settings);
+
+        if (array_key_exists('save_queries', $settings)) {
+            $this->saveQueries = (bool) $settings['save_queries'];
+        }
+
+        if (array_key_exists('permanent_connection', $settings)) {
+            $this->permanentConnection = (bool) $settings['permanent_connection'];
+        }
+
+        if (array_key_exists('report_error', $settings)) {
+            $this->setReportError($settings['report_error']);
+        }
+
+        if (array_key_exists('charset', $settings)) {
+            $this->setCharset($settings['charset']);
+        }
+
+        if (!is_array($settings['parameters'])) {
+            throw new Exception('"parameters" settings is not an array: ' . gettype($settings['parameters']), 20);
+        }
+
+        $this->parameters = $settings['parameters'];
+    }
+
+    /**
+     * @param array $settings
+     *
+     * @throws Exception
+     */
+    private function setMandatorySettings(array $settings)
+    {
+        $props = ['engine', 'host', 'user', 'password', 'database'];
+        foreach ($props as $prop) {
+            if (!isset($settings[$prop]) || !is_string($settings[$prop])) {
+                throw new Exception('"' . $prop . '" settings is not defined or not a string', 10);
+            }
+
+            $this->{'set'.ucfirst($prop)}($settings[$prop]);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getEngine()
+    {
+        return $this->engine;
+    }
+
+    /**
+     * @param string $engine
+     *
+     * @throws Exception
+     */
+    public function setEngine(string $engine)
+    {
+        $enginesAvailables = PDO::getAvailableDrivers();
+        if(!in_array($engine, $enginesAvailables))
+        {
+            throw new Exception('The engine "' . $engine . '" is not available for PDO', 20);
+        }
+
+        $this->engine = $engine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     */
+    public function setHost(string $host)
+    {
+        $this->host = $host;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param string $user
+     */
+    public function setUser(string $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    /**
+     * @param string $database
+     */
+    public function setDatabase(string $database)
+    {
+        $this->database = $database;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param mixed $key
+     * @param mixed $value
+     */
+    public function setParameter($key, $value)
+    {
+        $this->parameters[$key] = $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParametersForPDO()
+    {
+        $parameters = $this->getParameters();
+
+        if($this->getEngine() === 'mysql')
+        {
+            $parameters[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES ' . $this->getCharset();
+        }
+
+        if($this->getReportError() === 'silent')
+        {
+            $parameters[PDO::ATTR_ERRMODE] = PDO::ERRMODE_SILENT;
+        }
+        else if($this->getReportError() === 'exception')
+        {
+            $parameters[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+        }
+
+        $parameters[PDO::ATTR_PERSISTENT] = $this->permanentConnection;
+        
+        return $parameters;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasSaveQueries()
+    {
+        return $this->saveQueries;
+    }
+
+    public function enableSaveQueries()
+    {
+        $this->saveQueries = true;
+    }
+
+    public function disableSaveQueries()
+    {
+        $this->saveQueries = false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReportError()
+    {
+        return $this->reportError;
+    }
+
+    /**
+     * @param string $reportError
+     *
+     * @throws Exception
+     */
+    public function setReportError(string $reportError)
+    {
+        if(!in_array($reportError, ['silent', 'exception']))
+        {
+            throw new Exception('The report error "' . $reportError . '" is incorrect. (silent , exception)', 30);
+        }
+
+        $this->reportError = $reportError;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasThrowException()
+    {
+        return $this->reportError === 'exception';
+    }
+
+    /**
+     * @return string
+     */
+    public function getCharset()
+    {
+        return $this->charset;
+    }
+
+    /**
+     * @param string $charset
+     */
+    public function setCharset(string $charset)
+    {
+        $this->charset = $charset;
+    }
 }
--- src/Database.php
+++ src/Database.php
@@ -3,50 +3,686 @@
 namespace Rancoud\Database;
 
 use Exception;
+use PDO;
+use PDOException;
+use PDOStatement;
 
 /**
  * Class Database.
  */
 class Database
 {
-    /** @var Driver */
-    protected $driver;
+    /**
+     * @var Configurator
+     */
+    private $configurator = null;
+
+    /**
+     * @var PDO
+     */
+    private $pdo = null;
+
+    /**
+     * @var array
+     */
+    private $errors = [];
+
+    /**
+     * @var array
+     */
+    private $savedQueries = [];
+
+    /**
+     * @var Database
+     */
+    private static $instance;
 
     /**
      * Database constructor.
      *
      * @param Configurator $configurator
-     * @param Driver       $driver
      */
-    public function __construct(Configurator $configurator, Driver $driver)
+    public function __construct(Configurator $configurator)
     {
-        $this->driver = $driver;
-        /* @noinspection PhpUndefinedMethodInspection */
-        $this->driver->setConfigurator($configurator);
+        $this->configurator = $configurator;
     }
 
     /**
-     * @return Driver
+     * @param Configurator|null $configurator
+     *
+     * @return Database
+     * @throws Exception
      */
-    public function getDriver()
+    public static function getInstance(Configurator $configurator = null)
     {
-        return $this->driver->getDriver();
+        if (self::$instance === null) {
+            if($configurator === null)
+            {
+                throw new Exception('Configurator Missing', 10);
+            }
+            self::$instance = new self($configurator);
+        }
+        else if($configurator !== null) {
+            throw new Exception('Configurator Already Setup', 11);
+        }
+
+        return self::$instance;
     }
 
     /**
-     * @param $name
-     * @param $arguments
+     * @throws Exception
+     */
+    public function connnect()
+    {
+        $user = $this->configurator->getUser();
+        $password = $this->configurator->getPassword();
+        $parameters = $this->configurator->getParametersForPDO();
+        $dsn = $this->getDsn();
+
+        try {
+            $startTime = microtime(true);
+
+            if($this->configurator->getEngine() !== 'sqlite')
+            {
+                $this->pdo = new PDO($dsn, $user, $password, $parameters);
+            }
+            else
+            {
+                $this->pdo = new PDO($dsn, null, null, $parameters);
+            }
+
+            $endTime = microtime(true);
+
+            if($this->configurator->hasSaveQueries())
+            {
+                $this->savedQueries[] = ['Connection' => $this->getTime($startTime, $endTime)];
+            }
+        } catch (Exception $e) {
+            $this->addErrorConnection($e);
+
+            if($this->configurator->hasThrowException())
+            {
+                throw new Exception('Error Connecting Database', 20);
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getDsn()
+    {
+        $engine = $this->configurator->getEngine();
+        $host = $this->configurator->getHost();
+        $database = $this->configurator->getDatabase();
+        $charset = $this->configurator->getCharset();
+
+        $dsn = $engine . ':host=' . $host . ';dbname=' . $database . ';charset=' . $charset;
+        if($engine === 'sqlite')
+        {
+            $dsn = 'sqlite:' . $database . ';charset=' . $charset;
+        }
+
+        return $dsn;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
      *
      * @throws Exception
      *
-     * @return mixed
+     * @return PDOStatement
      */
-    public function __call($name, $arguments)
+    private function prepareBind($sql, $parameters = [])
     {
-        if (!method_exists($this->driver, $name)) {
-            throw new Exception('Method Invalid! : ' . $name);
+        if ($this->pdo === null) {
+            $this->connnect();
         }
 
-        return call_user_func_array([$this->driver, $name], $arguments);
+        $statement = $this->pdo->prepare($sql);
+        if ($statement === false) {
+            throw new Exception('Error Prepare Statement', 30);
+        }
+
+        foreach ($parameters as $key => $value) {
+            $param = $this->getPdoParamType($value);
+            if (is_float($value)) {
+                $value = (string) $value;
+            }
+
+            if ($param !== false) {
+                $statement->bindValue(":$key", $value, $param);
+            }
+        }
+
+        return $statement;
+    }
+
+    /**
+     * @param $value
+     *
+     * @return bool|int
+     */
+    private function getPdoParamType($value)
+    {
+        if (is_int($value)) {
+            return PDO::PARAM_INT;
+        } elseif (is_bool($value)) {
+            return PDO::PARAM_BOOL;
+        } elseif (null === $value) {
+            return PDO::PARAM_NULL;
+        } elseif (is_string($value)) {
+            return PDO::PARAM_STR;
+        } elseif (is_float($value)) {
+            return PDO::PARAM_STR;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param PDOStatement $statement
+     */
+    private function addError(PDOStatement $statement)
+    {
+        $this->errors[] = [
+            'query'       => $statement->queryString,
+            'query_error' => $statement->errorInfo(),
+            'pdo_error'   => $this->pdo->errorInfo(),
+            'dump_params' => $this->getDumpParams($statement)
+        ];
+    }
+
+    /**
+     * @param Exception $exception
+     */
+    private function addErrorConnection(Exception $exception)
+    {
+        $this->errors[] = [
+            'query'       => $this->getDsn(),
+            'query_error' => null,
+            'pdo_error'   => $exception->getMessage(),
+            'dump_params' => $this->configurator->getParametersForPDO()
+        ];
+    }
+
+    /**
+     * @param PDOStatement $statement
+     * @param array         $parameters
+     * @param               $time
+     */
+    private function saveQueries(PDOStatement $statement, $parameters, $time)
+    {
+        if ($this->configurator->hasSaveQueries()) {
+            $this->savedQueries[] = [
+                'query'      => $statement->queryString,
+                'parameters' => $parameters,
+                'time'       => $time
+            ];
+        }
+    }
+
+    /**
+     * @param PDOStatement $statement
+     *
+     * @return string
+     */
+    private function getDumpParams(PDOStatement $statement)
+    {
+        ob_start();
+        $statement->debugDumpParams();
+        $result = ob_get_contents();
+        ob_end_clean();
+
+        return $result;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @throws Exception
+     *
+     * @return PDOStatement
+     */
+    public function select($sql, $parameters = [])
+    {
+        $statement = $this->prepareBind($sql, $parameters);
+
+        $startTime = microtime(true);
+
+        try {
+            $results = $statement->execute();
+            if ($results === false) {
+                $this->addError($statement);
+                //throw new Exception('Error Execute Select Query', 30);
+            }
+        } catch (Exception $e) {
+            $this->addError($statement);
+        }
+
+        $endTime = microtime(true);
+
+        $this->saveQueries($statement, $parameters, $this->getTime($startTime, $endTime));
+
+        return $statement;
+    }
+
+    /**
+     * @param PDOStatement $statement
+     * @param null         $fetchType
+     *
+     * @return mixed
+     */
+    public function read($statement, $fetchType = null)
+    {
+        if ($fetchType === null) {
+            $fetchType = PDO::FETCH_ASSOC;
+        }
+
+        return $statement->fetch($fetchType);
+    }
+
+    /**
+     * @param PDOStatement $statement
+     * @param null          $fetchType
+     *
+     * @return array
+     */
+    public function readAll(PDOStatement $statement, $fetchType = null)
+    {
+        if ($fetchType === null) {
+            $fetchType = PDO::FETCH_ASSOC;
+        }
+
+        return $statement->fetchAll($fetchType);
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     * @param bool  $getLastInsertId
+     *
+     * @throws Exception
+     *
+     * @return int|null
+     */
+    public function insert($sql, $parameters = [], $getLastInsertId = false)
+    {
+        $statement = $this->prepareBind($sql, $parameters);
+
+        $startTime = microtime(true);
+
+        $results = $statement->execute();
+        if ($results === false) {
+            $this->addError($statement);
+            throw new Exception('Error Execute Insert Query', 40);
+        }
+
+        $endTime = microtime(true);
+
+        $this->saveQueries($statement, $parameters, $this->getTime($startTime, $endTime));
+        if ($getLastInsertId === true) {
+            return (int) $this->pdo->lastInsertId();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     * @param bool  $getCountRowAffected
+     *
+     * @throws Exception
+     *
+     * @return int|null
+     */
+    public function update($sql, $parameters = [], $getCountRowAffected = false)
+    {
+        $statement = $this->prepareBind($sql, $parameters);
+
+        $startTime = microtime(true);
+
+        $results = $statement->execute();
+        if ($results === false) {
+            $this->addError($statement);
+            throw new Exception('Error Execute Update Query', 50);
+        }
+
+        $endTime = microtime(true);
+
+        $this->saveQueries($statement, $parameters, $this->getTime($startTime, $endTime));
+        if ($getCountRowAffected) {
+            return (int) $statement->rowCount();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     * @param bool  $getCountRowAffected
+     *
+     * @throws Exception
+     *
+     * @return int|null
+     */
+    public function delete($sql, $parameters = [], $getCountRowAffected = false)
+    {
+        $statement = $this->prepareBind($sql, $parameters);
+
+        $startTime = microtime(true);
+
+        $results = $statement->execute();
+        if ($results === false) {
+            $this->addError($statement);
+            throw new Exception('Error Execute Delete Query', 60);
+        }
+
+        $endTime = microtime(true);
+
+        $this->saveQueries($statement, $parameters, $this->getTime($startTime, $endTime));
+        if ($getCountRowAffected) {
+            return (int) $statement->rowCount();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @return int
+     * @throws Exception
+     */
+    public function count($sql, $parameters = [])
+    {
+        $statement = $this->select($sql, $parameters);
+        $cursor = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return (int) current($cursor);
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @throws Exception
+     */
+    public function exec($sql, $parameters = [])
+    {
+        $statement = $this->prepareBind($sql, $parameters);
+
+        $startTime = microtime(true);
+
+        $results = $statement->execute();
+        if ($results === false) {
+            $this->addError($statement);
+            throw new Exception('Error Execute Exec Query', 70);
+        }
+
+        $endTime = microtime(true);
+
+        $this->saveQueries($statement, $parameters, $this->getTime($startTime, $endTime));
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function selectAll($sql, $parameters = [])
+    {
+        $cursor = $this->select($sql, $parameters);
+
+        return $this->readAll($cursor);
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public function selectRow($sql, $parameters = [])
+    {
+        $cursor = $this->select($sql, $parameters);
+        $row = $this->read($cursor);
+
+        return $row;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function selectCol($sql, $parameters = [])
+    {
+        $cursor = $this->select($sql, $parameters);
+        $datas = $this->readAll($cursor);
+        $col = [];
+        foreach ($datas as $data) {
+            $col[] = current($data);
+        }
+
+        return $col;
+    }
+
+    /**
+     * @param       $sql
+     * @param array $parameters
+     *
+     * @return mixed|null
+     * @throws Exception
+     */
+    public function selectVar($sql, $parameters = [])
+    {
+        $cursor = $this->select($sql, $parameters);
+        $row = $this->read($cursor);
+        if ($row === false) {
+            return null;
+        }
+
+        return current($row);
+    }
+
+    public function beginTransaction()
+    {
+        $this->pdo->beginTransaction();
+    }
+
+    public function commit()
+    {
+        $this->pdo->commit();
+    }
+
+    public function rollback()
+    {
+        $this->pdo->rollBack();
+    }
+
+    public function startTransaction()
+    {
+        $this->beginTransaction();
+    }
+
+    public function completeTransaction()
+    {
+        if ($this->hasErrors()) {
+            $this->commit();
+        } else {
+            $this->rollback();
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasErrors()
+    {
+        return empty($this->errors);
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getLastError()
+    {
+        $countErrors = count($this->errors);
+        if($countErrors === 0)
+        {
+            return null;
+        }
+
+        return $this->errors[$countErrors - 1];
+    }
+    
+    public function cleanErrors()
+    {
+        $this->errors = [];
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasSaveQueries()
+    {
+        return $this->configurator->hasSaveQueries();
+    }
+
+    public function enableSaveQueries()
+    {
+        $this->configurator->enableSaveQueries();
+    }
+
+    public function disableSaveQueries()
+    {
+        $this->configurator->disableSaveQueries();
+    }
+
+    public function cleanSavedQueries()
+    {
+        $this->savedQueries = [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getSavedQueries()
+    {
+        return $this->savedQueries;
+    }
+
+    /**
+     * @param $string
+     *
+     * @return mixed
+     */
+    private function cleanField($string)
+    {
+        return str_replace('`', '', $string);
+    }
+
+    /**
+     * @param $table
+     *
+     * @throws Exception
+     */
+    public function truncate($table)
+    {
+        $table = $this->cleanField($table);
+        $sql = 'TRUNCATE TABLE `' . $table . '`';
+        $this->exec($sql);
+    }
+
+    /**
+     * @param $tables
+     *
+     * @throws Exception
+     */
+    public function dropTable($tables)
+    {
+        if (is_array($tables)) {
+            $tables = array_map([$this, 'cleanField'], $tables);
+            $tables = implode('`,`', $tables);
+        } else {
+            $tables = $this->cleanField($tables);
+        }
+        $sql = 'DROP TABLE IF EXISTS `' . $tables . '`';
+        $this->exec($sql);
+    }
+
+    /**
+     * @param $tables
+     *
+     * @throws Exception
+     */
+    public function optimize($tables)
+    {
+        if (is_array($tables[0])) {
+            $tables = array_map([$this, 'cleanField'], $tables);
+            $tables = implode('`,`', $tables);
+        } else {
+            $tables = $this->cleanField($tables);
+        }
+        $sql = 'OPTIMIZE TABLE `' . $tables . '`';
+        $this->exec($sql);
+    }
+
+    /**
+     * @param $filepath
+     *
+     * @throws Exception
+     */
+    public function useSqlFile($filepath)
+    {
+        if (!file_exists($filepath)) {
+            throw new Exception('File missing for useSqlFile method: ' . $filepath, 80);
+        }
+
+        $sqlFile = file_get_contents($filepath);
+
+        $this->exec($sqlFile);
+    }
+
+    /**
+     * @param $startTime
+     * @param $endTime
+     *
+     * @return float|int
+     */
+    protected function getTime($startTime, $endTime)
+    {
+        return round(($endTime - $startTime) * 1000000) / 1000000;
+    }
+
+    public function disconnect(){
+        $this->pdo = null;
     }
 }
